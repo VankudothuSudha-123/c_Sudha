85c9289c60be20c6ffb541fdd7b7be6b63b2867d
VankudothuSudha-123 <106797112+VankudothuSudha-123@users.noreply.github.com>
2022-06-05 19:43:14 +0530

    Create Variable_Program5.c

---
+++ Variable_Program5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    // here int and float are data types
+    // a,b, and c are variables.
+    // Division of two numbers.
+   int a = 21;
+   float b = 21;
+   float c = a/b;
+   printf("%.1f",c);
+   return 0;
+}
