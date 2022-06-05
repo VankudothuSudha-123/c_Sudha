93d1f262d2ef1d08cba4fac7d4d02c5640ea48fb
VankudothuSudha-123 <106797112+VankudothuSudha-123@users.noreply.github.com>
2022-06-05 19:41:21 +0530

    Create Variable_Program4.c

---
+++ Variable_Program4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   
+    // here int and float are data types
+    // a,b, and c are variables.
+    // Multiplication of two numbers
+   int a = 21;
+   float b = 21;
+   float c = a*b;
+   printf("%.1f",c);
+   return 0;
+}
